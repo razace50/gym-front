161edfd2e5acebe567be574cfc1c325ffd8b7d63
Rajesh Sunar <134181496+razace50@users.noreply.github.com>
2025-03-20 21:50:40 +0545

    ..

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -13,37 +13,66 @@ const Navbar: React.FC = () => {
     { id: 4, name: "Contact", link: "/contact" },
   ];
 
+  const handleNavItemClick = () => {
+    // Close the menu when a nav item is clicked
+    setIsMenuOpen(false);
+  };
+
   return (
     <>
       <nav className="bg-orange-500 text-white px-6 py-2 flex justify-between items-center">
         <div className="flex items-center space-x-2">
           <img src={logo} className="h-[75px] w-[75px]" alt="logo" />
           <div className="flex flex-col space-y-0">
-            <h1 className="text-3xl font-semibold">Hamro</h1>{" "}
-            <span className=" text-3xl font-semibold"> Gym</span>
+            <h1 className="text-3xl font-semibold">Hamro</h1>
+            <span className="text-3xl font-semibold"> Gym</span>
           </div>
         </div>
+
         {/* Hamburger Icon for small devices */}
-        <div className="lg:hidden md:hidden sm:block sm:order-2 ">
+        <div className="lg:hidden sm:block order-2">
           <Button
             variant={"secondary"}
             onClick={() => setIsMenuOpen(!isMenuOpen)}
+            className="p-2"
           >
             <AlignJustify />
           </Button>
         </div>
-        <div className=" hidden lg:flex items-center justify-between space-x-12">
+
+        {/* Navigation Items for Large Devices */}
+        <div className="hidden lg:flex items-center justify-between space-x-12">
           {navItems.map((item, index) => (
             <a
               key={index}
               href={item.link}
-              className=" text-lg font-semibold hover:underline"
+              className="text-lg font-semibold hover:underline"
             >
               {item.name}
             </a>
           ))}
         </div>
-        <div className="sm:order-1">
+
+        {/* Show menu Items on Small Devices when Hamburger is Clicked */}
+        <div
+          className={`lg:hidden w-full bg-orange-500 ${
+            isMenuOpen ? "block" : "hidden"
+          } mt-4 hover:shadow-lg p-4`}
+        >
+          {navItems.map((item, index) => (
+            <a
+              key={index}
+              href={item.link}
+              className="block py-2 text-lg font-semibold hover:underline"
+              onClick={handleNavItemClick} // Close menu when an item is clicked
+            >
+              {item.name}
+            </a>
+          ))}
+        </div>
+
+        {/* Join Membership Button */}
+        <div className="order-1">
           <Button variant={"secondary"}>Join Membership</Button>
         </div>
       </nav>
