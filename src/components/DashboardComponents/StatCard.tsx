5e9ad91932e8305dd98add5a590f035ac1d79478
Rajesh Sunar <134181496+razace50@users.noreply.github.com>
2025-09-22 21:41:14 +0545

    statcards and dashboard codes updated

--- src/components/DashboardComponents/StatCard.tsx
+++ src/components/DashboardComponents/StatCard.tsx
@@ -3,12 +3,15 @@ import React from 'react'
 interface StatCardProps {
     title: string;
     value: string | number;
+    icon?: React.ReactNode;
+    color?: string;
 }
 
-export default function StatCard({title, value}: StatCardProps) {
+export default function StatCard({title, value, icon, color}: StatCardProps) {
   return (
-    <div className='bg-white shadow rounded p-4'>
-        <h3 className='text-gray-600'>{title}</h3>
+    <div className='bg-white shadow rounded p-6 flex flex-col items-center justify-center border'>
+      <div className={`text-4xl mb-3 ${color}`}>{icon}</div>
+        <h3 className='text-gray-600 font medium'>{title}</h3>
         <p className='text-2xl font-bold'>{value}</p>
     </div>
   );
---
+++ src/components/pages/Dashboard.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import StatCard from '../DashboardComponents/StatCard'
+
+export default function Dashboard() {
+  return (
+    <div>
+        <StatCard title='' value={}/>
+        <StatCard title='' value={}/>
+        <StatCard title='' value={}/>
+        <StatCard title='' value={}/>
+        <StatCard title='' value={}/>
+        <StatCard title='' value={}/>
+    </div>
+  )
+}
