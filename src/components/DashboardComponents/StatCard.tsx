1e9d447952f1365147eb48f70b98e57d229b359a
Rajesh Sunar <134181496+razace50@users.noreply.github.com>
2025-09-24 21:47:42 +0545

    code updated for classes

--- src/components/DashboardComponents/StatCard.tsx
+++ src/components/DashboardComponents/StatCard.tsx
@@ -9,7 +9,7 @@ interface StatCardProps {
 
 export default function StatCard({title, value, icon, color}: StatCardProps) {
   return (
-    <div className='bg-gray-300 shadow rounded p-6 flex flex-col items-center justify-center border-[6px] border-purple-500 rounded-2xl h-60 transition-transform duration-300 hover:scale-110 cursor-pointe'>
+    <div className='bg-gray-300 shadow rounded p-6 flex flex-col items-center justify-center border-[6px] border-purple-500 rounded-2xl h-60 transition-transform duration-300 hover:scale-110 cursor-pointer'>
       <div className={`text-4xl mb-3 ${color}`}>{icon}</div>
         <h3 className='text-gray-600 font-medium text-xl'>{title}</h3>
         <p className='text-2xl font-bold'>{value}</p>
